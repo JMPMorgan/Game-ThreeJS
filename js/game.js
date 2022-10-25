a886dc6e25db84f6ff687ac14edea7c9694e1afd
JMPMorgan <jonathan.morganp@gmail.com>
2022-10-25 14:19:58 -0500

    add models

--- js/game.js
+++ js/game.js
@@ -1,4 +1,23 @@
 const SCENE = new THREE.Scene();
+
+var keyLight = new THREE.DirectionalLight(
+  new THREE.Color("hsl(30, 100%, 75%)"),
+  1.0
+);
+keyLight.position.set(-100, 0, 100);
+
+var fillLight = new THREE.DirectionalLight(
+  new THREE.Color("hsl(240, 100%, 75%)"),
+  0.75
+);
+fillLight.position.set(100, 0, 100);
+
+var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+backLight.position.set(100, 0, -100).normalize();
+
+SCENE.add(keyLight);
+SCENE.add(fillLight);
+SCENE.add(backLight);
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -24,12 +43,31 @@ let cube = new THREE.Mesh(geometry, [
   material3,
 ]);
 
+let house;
+const objLoader = new THREE.OBJLoader();
+const mtlLoader = new THREE.MTLLoader();
+mtlLoader.setTexturePath("/assets/models/cabaña");
+mtlLoader.load("/assets/models/cabaña/house.mtl", (materials) => {
+  materials.preload();
+  objLoader.setMaterials(materials);
+});
+objLoader.setPath("/assets/models/cabaña/");
+objLoader.load("house.obj", (object) => {
+  //object.position.y -= 50;
+  object.position.z = -25;
+  house = object;
+  SCENE.add(object);
+});
+//mtlLoader.setTexturePath('/assets/models/');
+
 SCENE.add(cube);
 
 camera.position.z = 5;
 
 const animate = () => {
   requestAnimationFrame(animate);
+  house.rotation.x += 0.05;
+  house.rotation.y += 0.1;
   cube.rotation.x += 0.1;
   cube.rotation.y += 0.1;
   RENDERER.render(SCENE, camera);
